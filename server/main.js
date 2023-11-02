206d3d74a89ffa721a9b14062e0a96707fa4a018
Martin Elsässer <marels@aon.at>
2023-11-03 00:56:40 +0100

    added .env file

--- server/main.js
+++ server/main.js
@@ -1,7 +1,9 @@
 import { Meteor } from "meteor/meteor";
 import { TasksCollection } from "/imports/api/TasksCollection";
+require("dotenv").config();
 
 Meteor.startup(() => {
+	console.log(process.env.SECRET2);
 	if (TasksCollection.find().count() === 0) {
 		[
 			"First Task",
